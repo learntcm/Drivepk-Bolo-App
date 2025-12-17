b9cfa8c2eccba95780afc1f65fb00cdc52eb0a93
learntcm <63395778+learntcm@users.noreply.github.com>
2025-12-17 21:04:34 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-window.__APP_CONFIG__ = {
-    openAIKey: "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",
-    googleKey: "AIzaSyAzEgST_gBAhua-L0VZyiL6tryuQ-zaSRg" 
-};
+
